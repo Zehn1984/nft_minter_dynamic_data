92f3541fa66b93d88649f6b795ba92abb5c65b86
Anderson Campolina <andersoncampolina1@gmail.com>
2022-09-04 14:32:59 -0300

    Adicionei restante das funcoes e tratei varios erros. Acrescentei funcionalidade de autoDate e calculo automatico de custos

--- src/pages/minter/index.js
+++ src/pages/minter/index.js
@@ -9,31 +9,42 @@ const CallContract = () => {
   const abi = require("../../abiByteCode/abi.json")
 
   // Criando useState() react para retornar na tela a informacao coletada da blockchain
-  let [nome, setNome] = useState("")
   let [signer, setSigner] = useState(provider.getSigner())
   let [wallet, setWallet] = useState(localStorage.getItem("wallet"))
+  let [saldoInicial, setSaldoInicial] = useState(false)
   let [balance, setBalance] = useState("")
-  let [deployedContract, setDeployedContract] = useState(false)
+  let [deployedContract, setDeployedContract] = useState(localStorage.getItem("contract_address"))
   let [contractObject, setContractObject] = useState(new ethers.Contract(localStorage.getItem("contract_address"), abi, provider)) // ja comecamos com o ultimo contrato deployado na memoria
   let [balanceOf, setBalanceOf] = useState("")
-  let [textInput, setTextInput] = useState(wallet)
-
+  let [mintInput, setMintInput] = useState(wallet)
+  let [conquistaInput, setConquistaInput] = useState("")
+  let [conquistaAdicionada, setConquistaAdicionada] = useState("")
+  let [historico, setHistorico] = useState("")
+  let [gastoTaxas, setGastoTaxas] = useState(0)
+  
   // Conectar Carteira Metamask
   const connectMetamask = async () => {    
-    // Requisitando permissao ao usuario para conexao da metamask junto ao script e contrato
+    // Requisitando permissao ao usuario para conexao da metamask junto ao script e contrato    
     provider.send("eth_requestAccounts", []);
     signer = provider.getSigner(); // requisicao de assinatura para conectar na metamask, caso nao esteja conectado
     localStorage.setItem("wallet", await signer.getAddress()) // captura a wallet em um DB local
     setSigner(await signer) // atualiza estado do signer para conectado, assim podemos chamar as outras funcoes que exige assinatura
     setWallet(await signer.getAddress()) // Pega o endereco da carteira conectada
+    const balance_wei = await signer.getBalance()
+    balance = await ethers.utils.formatEther(balance_wei)
+    setSaldoInicial(balance)
+    getSaldo()
   }
 
-  // Pegar saldo matic
+  // Executa a funcao de saldo para saber quantos Matic e CNFT tem na carteira
   const getSaldo = async () =>  {
     const balance_wei = await signer.getBalance()
     balance = await ethers.utils.formatEther(balance_wei) // converte de wei pra ether (divide por 1e18)
     localStorage.setItem("balance", await balance)
     setBalance(await balance)
+    setBalanceOf(await contractObject.balanceOf(wallet))
+    let taxas = parseFloat(saldoInicial) - parseFloat(await balance)
+    setGastoTaxas(taxas)
   }
   
   // Minta carteirinha e guarda hash do endereco em uma variavel
@@ -51,82 +62,75 @@ const CallContract = () => {
     localStorage.setItem("name", await contractObject.name())
     localStorage.setItem("symbol", await contractObject.symbol())
     localStorage.setItem("balanceOf", await contractObject.balanceOf(wallet))
-    localStorage.setItem("lerHistoricoCarteirinha", await contractObject.lerHistoricoCarteirinha())
+    //localStorage.setItem("lerHistoricoCarteirinha", await contractObject.lerHistoricoCarteirinha())
 
     setDeployedContract(await deployedContract)
     setContractObject(await contractObject) // Agora ContractObject eh um objeto que interage com as funcoes do contrato. pra chamar a funcao de nome por exemplo basta usar contractObject.name()
+    getSaldo()
   }
 
-  // Executa a funcao de saldo para saber quantas CNFT tem na carteira
-  const getSaldoNft = async () =>  {
-    setBalanceOf(await contractObject.balanceOf(wallet))
-  }
-
-  const mintNft = async (props) => {
-    const transaction = await contractObject.connect(signer).safeMint(textInput) //executa a funcao de mint do contrato. Para funcoes que mudam o estado da blockchain, eh necessario uma assinatura confirmacao (signer)
+  // Minta o NFT a uma carteira, ou seja, define um dono ao NFT.
+  const mintNft = async () => {
+    const transaction = await contractObject.connect(signer).safeMint(mintInput) //executa a funcao de mint do contrato. Para funcoes que mudam o estado da blockchain, eh necessario uma assinatura confirmacao (signer)
     await transaction.wait()
+    getSaldo()
     //saldo_nft = await contractObject.balanceOf(textInput) // verifica se apos a mintagem, acrescentou 1 CNFT a carteira solicitada
   }
 
-  const userInput = (props) => {
-    setTextInput(props.target.value)
+  const aoDigitarMint = (props) => {
+    setMintInput(props.target.value)
   }
-  /*
-  // Minta o NFT a uma carteira, ou seja, define um dono ao NFT.
-  async function mintNft() {
 
-    // pega a carteira que tera o token mintado digitada no formulario e armazena na variavel wallet
-    const wallet = document.querySelector("#input_set_owner_wallet").value
-
-    const carteirinhaNFTcontract = new ethers.Contract(hash_contrato_deployado, abi, provider); // define carteirinhaNFTcontract como um objeto que interage com as funcoes do contrato CarteirinhaNFT.sol
-    
-    if (wallet === "") {
-      set_owner_wallet = document.querySelector("#set_owner_wallet")
-      set_owner_wallet.textContent = "Preencha o campo com alguma carteira para realizar o mint!";
-    } else {
-      const transacao = await carteirinhaNFTcontract.connect(signer).safeMint(wallet) //executa a funcao de mint do contrato. Para funcoes que mudam o estado da blockchain, eh necessario uma assinatura confirmacao (signer)
-      await transacao.wait()
-      saldo_nft = await carteirinhaNFTcontract.balanceOf(wallet) // verifica se apos a mintagem, acrescentou 1 CNFT a carteira solicitada
-      set_owner_wallet = document.querySelector("#set_owner_wallet") 
-      set_owner_wallet.textContent = `A carteira ${wallet} agora possui ${saldo_nft} CNFT. Para ver na metamask, adicione o token ${hash_contrato_deployado}`;
-      document.querySelector("#input_transfer_from_sender").value = wallet
-    }
+  const adicionarConquista = async () => {
+    const data = parseInt(`${new Date().getDate()}${new Date().getMonth()}${new Date().getFullYear()}`) // captura a data do momento para adicionar junto a blockchain nos parametros da funcao adicionarConquista
+    const transaction = await contractObject.connect(signer).adicionarConquistaHistorico(conquistaInput.toString(), data, 0)
+    await transaction.wait()
+    setConquistaAdicionada(true)
+    getSaldo()
   }
-  */
 
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  async function getNome() {
-    setNome(await contractObject.name())
+  const aoDigitarConquista = (props) => {
+    setConquistaInput(props.target.value)
+  }
+
+  // Ler historico da carteirinha
+  async function getHistorico() {
+    setHistorico(await contractObject.consultarHistorico())
   }
 
   return (
     <div className="App">
       <button onClick={connectMetamask}>
-      {wallet ? "Conectado: " + String(wallet) : "Conectar"}
+        {saldoInicial ? "Conectado: " + String(wallet): "Conectar"}
       </button>
       <br></br>
       <br></br>
 
-      <button onClick={getSaldo}>Saldo Matic</button>
-      <p>{String(balance)}</p>
-      <br></br>
+      <button onClick={getSaldo}>SALDO</button>
+      <p>{"Matic: " + String(balance) + " | R$ " + parseFloat(balance * 0.90 * 5).toFixed(2)}</p>
+      <p>CNFT: </p>
+      <h1>{String(balanceOf)}</h1>
 
       <button onClick={deployContract}>Deployar Contrato</button>
-      <div>{deployedContract ? `Sucesso! link do comprovante: https://polygonscan.com/address/${deployedContract}` : `Clique no botao acima para fazer o deploy do contrato direto na blockchain...`}</div>
+      <div>{"Ultimo Contrato deployado: " + String(deployedContract)}</div>
       <br></br>
 
-      <button onClick={getSaldoNft}>Saldo CNFT</button>
-      <p>{String(balanceOf)}</p>
-      <br></br>
-
-      <input type="text" placeholder="Digite a carteira para o mint..." value={textInput} onChange={userInput}></input>
+      <input type="text" placeholder="Digite a carteira para o mint..." value={mintInput} onChange={aoDigitarMint}></input>
       <br></br>
       <button onClick={mintNft}>Mint CNFT</button>
+      <br/>
+      <br/>
+      
+      <input type="text" placeholder="Digite a conquista..." value={conquistaInput} onChange={aoDigitarConquista}></input>
+      <br></br>
+      <button onClick={adicionarConquista}>Adicionar Conquista</button>
+      <p>{conquistaAdicionada ? "Conquista adicionada com sucesso!" : ""}</p>
 
-      <br></br>
-      <br></br>
-      <button onClick={getNome}>Nome do Token</button>
-      <p>{nome}</p>
+      <button onClick={getHistorico}>Ver historico de conquistas</button><br/>
+      <p>{String(historico)}</p>
+
+      <h1>Custo Total: {gastoTaxas ? "R$ " + (gastoTaxas * 0.90 * 5).toFixed(2) : 0}</h1>
+
     </div>
   );  
 }
